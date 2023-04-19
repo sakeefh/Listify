d9f98e6432cdc9e77124b8a9e575da2f77c7a072
Sakeef Hassan <91638600+sakeefh@users.noreply.github.com>
2023-04-19 16:23:20 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './main.css';
+import image1 from './images/house.png';
+import image2 from './images/school.png';
 
 function Main(props) {
   const [cards, setCards] = useState([]);
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(-1);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   function handleAddCard() {
     setCards([...cards, '']);
@@ -19,15 +24,44 @@ function Main(props) {
   }
 
   function handleLogout() {
-    props.onLogout();
+    props.handleLogout();
+  }
+  
+  
+
+  function handleShowImage(index, type) {
+    setSelectedButtonIndex(index);
+    if (type === 'personal') {
+      setSelectedImage(image1);
+    } else if (type === 'school') {
+      setSelectedImage(image2);
+    }
+  }
+
+  function handleHideButtons() {
+    setSelectedButtonIndex(-1);
+    setSelectedImage(null);
   }
 
   return (
     <div className="Main">
-      <h1>My Cards</h1>
-      <button onClick={handleAddCard}>Add Card</button>
+      <h1>My List</h1>
+      <button onClick={handleAddCard}>Add List</button>
       {cards.map((card, index) => (
         <div key={index} className="card">
+          {(selectedButtonIndex !== index) && (
+            <>
+              <button onClick={() => handleShowImage(index, 'personal')}>Personal</button>
+              <button onClick={() => handleShowImage(index, 'school')}>School</button>
+            </>
+          )}
+          {(selectedButtonIndex === index && selectedImage !== null) && (
+            <>
+              <img src={selectedImage} alt="Selected Image" />
+              <button onClick={handleHideButtons}>Back</button>
+            </>
+          )}
+          <div className="card-buttons"></div>
           <textarea
             value={card}
             onChange={(event) => handleUpdateCard(index, event.target.value)}
@@ -35,7 +69,9 @@ function Main(props) {
           <button onClick={() => handleDeleteCard(index)}>Delete Card</button>
         </div>
       ))}
-      <button className="logout" onClick={handleLogout}>Logout</button>
+                <button className="logout" onClick={handleLogout}>
+                  <Link to="/">Logout </Link>
+                </button>
     </div>
   );
 }
